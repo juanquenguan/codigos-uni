1a64471f75ba3cdaa7ba5aeadeca2cd6a59e8540
JuanQuenguan <juan.quenguan@correounivalle.co>
2025-09-19 10:11:54 -0500

    first commit

---
+++ act2.py
@@ -0,0 +1,36 @@
+"""
+Funcion parta calcular la suma de dos matrices cuadradas
+"""
+def suma(matrizA, matrizB):
+    matrizResultante = []
+    for i in(len(matrizA)):
+        fila = []
+        for j in (len(matrizA[0])):
+            fila.append(matrizA[i][j]  + matrizB[i][j])
+        matrizResultante.append(fila)
+    return matrizResultante
+
+"""
+Funcion parta calcular la resta de dos matrices cuadradas
+"""
+def resta(matrizA, matrizB):
+    matrizResultante = []
+    for i in(len(matrizA)):
+        fila = []
+        for j in (len(matrizA[0])):
+            fila.append(matrizA[i][j]  - matrizB[i][j])
+        matrizResultante.append(fila)
+    return matrizResultante
+
+
+"""
+Funcion parta calcular la adjunta de una matriz cuadrada
+"""
+def adjunta(matriz):
+    matrizResultante = []
+    for i in (len(matriz)):
+        fila = []
+        for j in (len(matriz)):
+            fila.append((-1)**(i+j)*determinante(menorIJ(matriz,i,j)))
+        matrizResultante.append(fila)
+    return matrizResultante
